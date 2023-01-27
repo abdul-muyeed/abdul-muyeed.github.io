ea2cca326c406b9b3740f7f1dab6c5055f461683
AbdulMuyeed47 <abdulmuyeed47@gmail.com>
2023-01-27 15:04:57 +0600

    new

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector("#menu").addEventListener("click", () => {
+  document.querySelector("#menu").classList.toggle("cross");
+  document.querySelector(".nav").classList.toggle("change");
+});
