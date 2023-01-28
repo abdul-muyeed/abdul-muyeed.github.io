066ebc99c4ac3530656456e4ad2914b84107b5ce
AbdulMuyeed47 <abdulmuyeed47@gmail.com>
2023-01-28 19:36:59 +0600

    new

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+window.addEventListener('touchmove', (e) => {
+  console.log(e);
+},{passive: true});
 document.querySelector("#menu").addEventListener("click", () => {
   document.querySelector("#menu").classList.toggle("cross");
   document.querySelector(".nav").classList.toggle("change");
 });
+
+
