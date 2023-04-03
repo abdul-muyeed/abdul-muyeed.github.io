d7dd552307b8d3deaaacd8f36ce9e2f15a24f76e
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-04-03 23:00:13 +0600

    Worked on the hero section

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
-window.addEventListener('scroll', ()=> {
-    const header = document.querySelector('header');
-  header.classList.toggle('sticky-header', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky-header", window.scrollY > 0);
+});
+let dtext = document.querySelector(".d-text");
+document.addEventListener("DOMContentLoaded", function () {
+  var dataText = ["YouTuber", "Web Developer", "Freelancer", "App Developer"];
+
+  function typeWriter(text, i, fnCallback) {
+    if (i < text.length) {
+      dtext.innerHTML = text.substring(0, i + 1);
+
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback);
+      }, 200);
+    } else if (typeof fnCallback == "function") {
+      setTimeout(fnCallback, 700);
+    }
+  }
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == "undefined") {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 2000);
+    }
+    if (i < dataText[i].length) {
+      typeWriter(dataText[i], 0, function () {
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  StartTextAnimation(0);
 });
